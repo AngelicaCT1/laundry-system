7deecb4e03782714b19d450e23d8f722a47350d6
Rapiwash <sistemarapiwash@gmail.com>
2024-06-02 13:30:30 -0500

    habilitar factura

--- src/services/global.js
+++ src/services/global.js
@@ -1,6 +1,6 @@
 export const timeZone = "America/Cancun"; //
 // Factura
-export const showFactura = false;
+export const showFactura = true;
 // Tipo de Moneda
 export const tipoMoneda = "MXN"; // PEN
 // Formato de la moneda (separacion de miles - decimales) segun la configuracion regional
